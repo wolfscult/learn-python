e0e62196508c2e43fbe2057bc3165102fb2aedf7
wolfscult <142390672+wolfscult@users.noreply.github.com>
2024-04-03 10:41:27 +0200

    New sals shipping.py
    Wrap control-flow in functions from the start. Hope it's not a problem

--- 2-control-flow/sals-shipping/shipping.py
+++ 2-control-flow/sals-shipping/shipping.py
@@ -1,36 +1,44 @@
-# Sal's Shipping
-# Sonny Li
+weight = 4.8
+ground_shiping_premium = 125.00
 
-weight = 80
+#ground shipping
 
-# Ground Shipping 🚚
+def ground_shipping():
+  if weight <=2:
+    cost =  weight * 1.50 + 20.00
+    return (cost)
+  elif weight >2 or weight <= 6:
+    cost =  weight * 3.00 + 20.00
+    return (cost)
+  elif weight > 6 or weight <= 10:
+    cost =  weight * 4.00 + 20.00
+    return (cost)
+  elif weight > 10:
+    cost =  weight * 4.75 + 20.00
+    return (cost)
+  else:
+    print('Error')
 
-if weight <= 2:
-  cost_ground = weight * 1.5 + 20
-elif weight <= 6:
-  cost_ground = weight * 3.00 + 20
-elif weight <= 10:
-  cost_ground = weight * 4.00 + 20
-else:
-  cost_ground = weight * 4.75 + 20
+#Drone shipping
 
-print("Ground Shipping $", cost_ground)
-      
-# Ground Shipping Premimum 🚚💨
+def drone_shipping():
 
-cost_ground_premium = 125.00
+  if weight <=2:
+    cost =  weight * 4.50
+    return (cost)
+  elif weight >2 or weight <= 6:
+    cost =  weight * 9.00
+    return (cost)
+  elif weight > 6 or weight <= 10:
+    cost =  weight * 12.00
+    return (cost)
+  elif weight > 10:
+    cost =  weight * 24.25
+    return (cost)
+  else:
+    print('Error')
 
-print("Ground Shipping Premimium $", cost_ground_premium)
-
-# Drone Shipping 🛸
-
-if weight <= 2:
-  cost_drone = weight * 4.5
-elif weight <= 6:
-  cost_drone = weight * 9.00
-elif weight <= 10:
-  cost_drone = weight * 12.00
-else:
-  cost_drone = weight * 14.25
-
-print("Drone Shipping: $", cost_drone)
+#Dispaying
+print("Your ground shipping cost is: " + str(ground_shipping()) + '$')
+print("Your premium shipping cost is: " + str(ground_shiping_premium) +'$')
+print("Your drone shipping cost is: " + str(round(drone_shipping(), 1)) + '$')
